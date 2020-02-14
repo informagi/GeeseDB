7a0516b0d15a96242e8a7fda8045b0d7f376f7ec
chriskamphuis <mail@chriskamphuis.com>
2020-02-14 14:36:35 +0100

    Add url to setup
    Updated setup.py file to include the github's url

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(
-   name='geesedb',
-   version='0.0.1',
-   description='Graph Engine for Exploration and Search over Evolving DataBases',
-   author='Chris Kamphuis',
-   author_email='chris@cs.ru.nl',
-   packages=['geesedb'],
-   install_requires=['duckdb'],
+    name='geesedb',
+    version='0.0.1',
+    description='Graph Engine for Exploration and Search over Evolving DataBases',
+    author='Chris Kamphuis',
+    author_email='chris@cs.ru.nl',
+    packages=['geesedb'],
+    url='https://github.com/informagi/GeeseDB',
+    install_requires=['duckdb'],
 )
