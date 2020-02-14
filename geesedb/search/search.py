9638c1c5e6427e881a0ade8f87dcce5551b56197
chriskamphuis <mail@chriskamphuis.com>
2020-02-14 14:28:21 +0100

    Set up for search class
    create a connection with a specified database

---
+++ geesedb/search/search.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3.6
+
+import argparse
+from geesedb.connection.connection import DBConnection
+
+
+class Searcher:
+
+    @staticmethod
+    def _parse_search_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--database', required=True, help='Name of the database / index')
+        return parser.parse_args()
+
+    def __init__(self):
+        self.args = self._parse_search_arguments()
+        self.db_connection = DBConnection(self.args.database)
+
+
+if __name__ == '__main__':
+    Searcher()
