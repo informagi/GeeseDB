63db292022458c0a7d9f922281d3ba166b8ecabe
chriskamphuis <mail@chriskamphuis.com>
2020-02-18 14:07:58 +0100

    Formatting + some restructuring
    * Also close the writers when done
    * Add paper as reference

--- geesedb/search/search.py
+++ geesedb/search/search.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3.6
 
 import argparse
+
 from geesedb.connection.connection import DBConnection
 
 
--- geesedb/utils/ciff/to_csv.py
+++ geesedb/utils/ciff/to_csv.py
@@ -1,12 +1,16 @@
 #! /usr/bin/env python3.6
 
-import geesedb.index.ciff.ciff_pb2
 import argparse
 import sys
 
+import geesedb.utils.ciff.ciff_pb2
+
 
 class ToCSV:
-
+    """
+    Class for creating csv files that represent tables in the old dog paper:
+    - https://dl.acm.org/doi/10.1145/2600428.2609460
+    """
     def __init__(self, cl_args):
         self.postings_file = cl_args.protobuf_file
         self.docs_file = cl_args.docs_file
@@ -30,7 +34,7 @@ class ToCSV:
                 return result, pos
             shift += 7
             if shift >= 64:
-                raise IOError('Too many bytes when decoding varint.')
+                raise IOError('Too many bytes when decoding.')
 
     def create_term_files(self, out_term_dict, out_term_doc):
         with open(self.postings_file, 'rb') as f:
@@ -42,51 +46,53 @@ class ToCSV:
             next_pos, pos = 0, 0
             term_id = 0
             while pos < len(data):
-                posting_list = geesedb.index.ciff.ciff_pb2.PostingsList()
+                posting_list = geesedb.utils.ciff.ciff_pb2.PostingsList()
                 next_pos, pos = self.decode(data, pos)
-                posting_list.ParseFromString(data[pos: pos+next_pos])
+                posting_list.ParseFromString(data[pos: pos + next_pos])
                 pos += next_pos
 
-                term_dict_writer.write(f'{term_id}|{posting_list.df}|{posting_list.term}')
+                term_dict_writer.write(f'{term_id}|{posting_list.df}|{posting_list.term}\n')
                 for posting in posting_list.posting:
-                    term_doc_writer.write(f'{term_id}|{posting.docid}|{posting.tf}')
+                    term_doc_writer.write(f'{term_id}|{posting.docid}|{posting.tf}\n')
                 term_id += 1
+            term_dict_writer.close()
+            term_doc_writer.close()
 
     def create_doc_file(self, out_doc):
         with open(self.docs_file, 'r') as f:
             doc_writer = open(out_doc, 'w')
             for line in f:
                 doc_id, trec_id, length = line.strip().split("\t")
-                doc_writer.write(f'{trec_id}|{doc_id}|{length}')
+                doc_writer.write(f'{trec_id}|{doc_id}|{length}\n')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--protobuf_file',
-                        '-p',
+    parser.add_argument('-p',
+                        '--protobuf_file',
                         required='--output_term_dict' in sys.argv and
                                  '--output_term_doc' in sys.argv,
                         metavar='[file]',
                         help='Location of the protobuf file, if this is included ' +
                              'output files for term related files should also be specified.')
-    parser.add_argument('--docs_file',
-                        '-d',
+    parser.add_argument('-d',
+                        '--docs_file',
                         required='--output_term_doc' in sys.argv,
                         metavar='[file]',
                         help='Location of the document metadata file, if this is included ' +
                              'the output file for documents should also be specified.')
-    parser.add_argument('--output_docs',
-                        '-od',
+    parser.add_argument('-o',
+                        '--output_docs',
                         required='--docs_file' in sys.argv,
                         metavar='[file]',
                         help='Output csv file for the docs table.')
-    parser.add_argument('--output_term_dict',
-                        '-ot',
+    parser.add_argument('-t',
+                        '--output_term_dict',
                         required='--protobuf_file' in sys.argv,
                         metavar='[file]',
                         help='Output csv file for the term dictionary table.')
-    parser.add_argument('--output_term_doc',
-                        '-oe',
+    parser.add_argument('-e',
+                        '--output_term_doc',
                         required='--protobuf_file' in sys.argv,
                         metavar='[file]',
                         help='Output csv file for the term doc mapper table.')
