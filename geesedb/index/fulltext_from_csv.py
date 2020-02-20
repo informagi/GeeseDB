ac8bdb1807fa21401b653d7176d23e8555594828
chriskamphuis <mail@chriskamphuis.com>
2020-02-20 14:49:52 +0100

    First test
    First test implemented
    Arguments refactor in csv class

--- geesedb/index/fulltext_from_csv.py
+++ geesedb/index/fulltext_from_csv.py
@@ -3,6 +3,7 @@
 import argparse
 import duckdb
 import os
+import sys
 
 from ..connection import DBConnection
 
@@ -18,8 +19,9 @@ class FullTextFromCSV:
         ['INT', 'INT', 'INT']
     ]
 
-    def __init__(self, arguments):
-        self.arguments = arguments
+    def __init__(self, args=sys.argv[1:]):
+        parser = self.parse_args()
+        self.arguments = parser.parse_args(args)
         if self.arguments.use_existing_db and os.path.isfile(self.arguments.database) or \
                 not self.arguments.use_existing_db and not os.path.isfile(self.arguments.database):
             pass
@@ -27,7 +29,7 @@ class FullTextFromCSV:
             raise IOError('There already exist a file on this path.')
         else:
             raise IOError('Database does not exist.')
-        db_connection = DBConnection(arguments.database)
+        db_connection = DBConnection(self.arguments.database)
         self.connection = db_connection.connection
         self.cursor = db_connection.cursor
 
@@ -75,69 +77,72 @@ class FullTextFromCSV:
         query = f"COPY {table_name} FROM '{file_name}' WITH DELIMITER '{delimiter}';"
         self.cursor.execute(query)
 
+    @staticmethod
+    def parse_args():
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-d',
+                            '--database',
+                            required=True,
+                            metavar='[file]',
+                            help='Location of the database.')
+        parser.add_argument('-u',
+                            '--use_existing_db',
+                            action='store_true',
+                            help='Use an existing database.')
+        parser.add_argument('-s',
+                            '--use_existing_tables',
+                            action='store_true',
+                            help='Use existing tables.')
+        parser.add_argument('-t',
+                            '--table_names',
+                            metavar='[string]',
+                            nargs=3,
+                            help='Decide on the table names you want to fill if they exist, ' +
+                                 'or create and fill them if they do not exist. If no names ' +
+                                 'are given the default values ["docs.csv", "term_dict.csv", ' +
+                                 '"term_doc.csv"] are being used. If arguments are given ' +
+                                 'they are expected in the respective default order.',
+                            default=['docs', 'term_dict', 'term_doc'])
+        parser.add_argument('-cd',
+                            '--columns_names_docs',
+                            metavar='[string]',
+                            nargs=2,
+                            default=['doc_id', 'collection_id', 'len'],
+                            help='Column names for the docs table.')
+        parser.add_argument('-ct',
+                            '--columns_names_term_dict',
+                            metavar='[string]',
+                            nargs=3,
+                            default=['term_id', 'df', 'string'],
+                            help='Column names for the dict table.')
+        parser.add_argument('-o',
+                            '--columns_names_term_doc',
+                            metavar='[string]',
+                            nargs=3,
+                            default=['term_id', 'doc_id', 'tf'],
+                            help='Column names for the term-docs table (docs in old dog paper).')
+        parser.add_argument('-di',
+                            '--docs_file',
+                            default='docs.csv',
+                            metavar='[file]',
+                            help='Filename for the csv file containing the data for the docs table.')
+        parser.add_argument('-ti',
+                            '--term_dict_file',
+                            default='dict.csv',
+                            metavar='[file]',
+                            help='Filename for the csv file containing the data for the dict table.')
+        parser.add_argument('-oi',
+                            '--term_doc_file',
+                            default='term_docs.csv',
+                            metavar='[file]',
+                            help='Filename for the csv file containing the data for the term-docs table ' +
+                                 '(docs in old dog paper).')
+        parser.add_argument('-e',
+                            '--delimiter',
+                            default='|',
+                            help='Delimiter that separates the columns in the csv files.')
+        return parser
+
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-d',
-                        '--database',
-                        required=True,
-                        metavar='[file]',
-                        help='Location of the database.')
-    parser.add_argument('-u',
-                        '--use_existing_db',
-                        action='store_true',
-                        help='Use an existing database.')
-    parser.add_argument('-s',
-                        '--use_existing_tables',
-                        action='store_true',
-                        help='Use existing tables.')
-    parser.add_argument('-t',
-                        '--table_names',
-                        metavar='[string]',
-                        nargs=3,
-                        help='Decide on the table names you want to fill if they exist, ' +
-                             'or create and fill them if they do not exist. If no names ' +
-                             'are given the default values ["docs.csv", "term_dict.csv", ' +
-                             '"term_doc.csv"] are being used. If arguments are given ' +
-                             'they are expected in the respective default order.',
-                        default=['docs', 'term_dict', 'term_doc'])
-    parser.add_argument('-cd',
-                        '--columns_names_docs',
-                        metavar='[string]',
-                        nargs=2,
-                        default=['doc_id', 'collection_id', 'len'],
-                        help='Column names for the docs table.')
-    parser.add_argument('-ct',
-                        '--columns_names_term_dict',
-                        metavar='[string]',
-                        nargs=3,
-                        default=['term_id', 'df', 'string'],
-                        help='Column names for the dict table.')
-    parser.add_argument('-o',
-                        '--columns_names_term_doc',
-                        metavar='[string]',
-                        nargs=3,
-                        default=['term_id', 'doc_id', 'tf'],
-                        help='Column names for the term-docs table (docs in old dog paper).')
-    parser.add_argument('-di',
-                        '--docs_file',
-                        default='docs.csv',
-                        metavar='[file]',
-                        help='Filename for the csv file containing the data for the docs table.')
-    parser.add_argument('-ti',
-                        '--term_dict_file',
-                        default='dict.csv',
-                        metavar='[file]',
-                        help='Filename for the csv file containing the data for the dict table.')
-    parser.add_argument('-oi',
-                        '--term_doc_file',
-                        default='term_docs.csv',
-                        metavar='[file]',
-                        help='Filename for the csv file containing the data for the term-docs table ' +
-                             '(docs in old dog paper).')
-    parser.add_argument('-e',
-                        '--delimiter',
-                        default='|',
-                        help='Delimiter that separates the columns in the csv files.')
-    args = parser.parse_args()
-    FullTextFromCSV(args)
+    FullTextFromCSV()
--- geesedb/tests/index/test_data_loading.py
+++ geesedb/tests/index/test_data_loading.py
@@ -1,2 +1,18 @@
+from ...index import FullTextFromCSV
+from os import path
+
+
 def test_load_csv():
-    assert 1 == 1
+    db = FullTextFromCSV(args=['-d',
+                               ':memory:',
+                               '-di',
+                               path.dirname(path.dirname(__file__)) + '/resources/example_files/example_docs.csv',
+                               '-ti',
+                               path.dirname(path.dirname(__file__)) + '/resources/example_files/example_term_dict.csv',
+                               '-oi',
+                               path.dirname(path.dirname(__file__)) + '/resources/example_files/example_term_doc.csv'
+                               ]
+                         )
+    db.cursor.execute("SELECT * FROM docs;")
+    assert db.cursor.fetchone() == ['document_0', 0, 3]
+    assert db.cursor.fetchone() == ['document_1', 1, 4]
