97fbf42de6e3f445c059082988561bdc12fa9c4e
chriskamphuis <mail@chriskamphuis.com>
2021-06-07 16:15:01 +0200

    Add searcher to init file

--- geesedb/search/__init__.py
+++ geesedb/search/__init__.py
@@ -1,3 +1,4 @@
 from .retrieval_models.bag_of_words.disjunctive.robertson_bm25 import RobertsonBM25
+from .search import Searcher
 
-__all__ = ['RobertsonBM25']
+__all__ = ['RobertsonBM25', 'Searcher']
