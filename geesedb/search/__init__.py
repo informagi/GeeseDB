c5b9ad12d82989a2b1dfcd6fbda71795d2ed22ca
chriskamphuis <mail@chriskamphuis.com>
2021-06-22 17:18:54 +0200

    Rename search.py to searcher.py

--- geesedb/search/__init__.py
+++ geesedb/search/__init__.py
@@ -1,4 +1,4 @@
 from .retrieval_models.bag_of_words.disjunctive.robertson_bm25 import RobertsonBM25
-from .search import Searcher
+from .searcher import Searcher
 
 __all__ = ['RobertsonBM25', 'Searcher']
