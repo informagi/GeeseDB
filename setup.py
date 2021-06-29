0fd395052a7428116215e6c539325051ccf19c22
chriskamphuis <mail@chriskamphuis.com>
2021-06-29 23:16:40 +0200

    prepare for v0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='geesedb',
-    version='0.0.1',
+    version='0.0.2',
     description='Graph Engine for Exploration and Search over Evolving DataBases',
     author='Chris Kamphuis',
     author_email='chris@cs.ru.nl',
