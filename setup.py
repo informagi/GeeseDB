8c918976fa589bb6f0074e58d0955cf964e6dc4a
Chris Kamphuis <mail@chriskamphuis.com>
2021-09-02 11:04:37 +0200

    Update dependencies for new parser

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author='Chris Kamphuis',
     author_email='chris@cs.ru.nl',
     url='https://github.com/informagi/GeeseDB',
-    install_requires=['duckdb', 'numpy', 'pandas'],
+    install_requires=['duckdb', 'numpy', 'pandas', 'protobuf', 'pycypher @ git+git://github.com/informagi/pycypher'],
     packages=find_packages(),
     include_package_data=True,
     package_data={'': ['qrels.*', 'topics.*']},
